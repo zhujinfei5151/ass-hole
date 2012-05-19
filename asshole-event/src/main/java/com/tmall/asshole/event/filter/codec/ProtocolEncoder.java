6f5cd07500cad905c34201daa13849e9b36495e1
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-05-19 06:58:43 +0000

    增加JSON数据解析器

--- asshole-event/src/main/java/com/tmall/asshole/event/filter/codec/ProtocolEncoder.java
+++ asshole-event/src/main/java/com/tmall/asshole/event/filter/codec/ProtocolEncoder.java
@@ -3,5 +3,5 @@ package com.tmall.asshole.event.filter.codec;
  * @author tangjinou
  */
 public interface ProtocolEncoder<T> {
-	byte[] encode( T t) throws Exception;
+	byte[] encode( T t, Class<? extends T> clz) throws Exception;
 }

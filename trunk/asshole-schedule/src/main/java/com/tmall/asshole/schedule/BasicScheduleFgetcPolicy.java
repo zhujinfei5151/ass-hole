d4c0c1e16990057b1c95fea96cabe2277e044198
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-03 07:20:04 +0000

    增加基本调度算法

--- trunk/asshole-schedule/src/main/java/com/tmall/asshole/schedule/BasicScheduleFgetcPolicy.java
+++ trunk/asshole-schedule/src/main/java/com/tmall/asshole/schedule/BasicScheduleFgetcPolicy.java
@@ -1,23 +1,46 @@
 package com.tmall.asshole.schedule;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tmall.asshole.zkclient.INodeChange;
+import com.tmall.asshole.zkclient.ZKConfig;
+
 /******
- * 
- *  基本抓取算法
- *  
  * 
  * @author tangjinou
  *
  */
-public class BasicScheduleFgetcPolicy implements IScheduleFgetcPolicy , Watcher{
+public class BasicScheduleFgetcPolicy implements IScheduleFgetcPolicy , INodeChange{
 	
 	private int startIndex;
 	
 	private int endIndex;
 	
-	private int rownum;
+	private int rownum=1000;
+	
+	private int max_hash_num=10000;
+	
+	@Autowired
+	private ZKConfig zKConfig;
+	
+	private List<String> _machines=new ArrayList<String>();
+	
+	private Lock lock = new ReentrantLock();
 
+	
+	public BasicScheduleFgetcPolicy(ZKConfig zKConfig){
+		 this.zKConfig = zKConfig;
+	}
+	public BasicScheduleFgetcPolicy() {
+	}
+	
 	public int getStartIndex() {
 		return startIndex;
 	}
@@ -28,12 +51,47 @@ public class BasicScheduleFgetcPolicy implements IScheduleFgetcPolicy , Watcher{
 	public int getRowNum() {
 		return rownum;
 	}
-	
-	public void process(WatchedEvent event){
-		   //有的机器上线状态
-		
-		
+
+	public void onChange(List<String> machines) {
+	      Collections.sort(machines);
+	      try{
+	      lock.lock();
+	      
+	      for (String ip : machines) {
+			    if(!_machines.contains(ip)){
+			    	_machines.clear();
+			    	_machines.addAll(machines);
+			    	onChange();
+			    }
+		  }
+	      }catch (Exception e) {
+	    	  
+		 }
+	      finally{
+	    	  lock.unlock();
+	      }
 	}
+
+	private void onChange() {
+		int machine_num = _machines.size();
+		int index = _machines.indexOf(zKConfig.getLocalIPAddress());
+		int hash_range= max_hash_num/machine_num;
+		
+		if((machine_num-1)==index){
+			//最后一台机器
+			startIndex =  index * hash_range;
+			endIndex =max_hash_num;
+		}else{
+			startIndex = index * hash_range;
+			endIndex = (index+1) * hash_range -1;
+		}
+		
+		
+		  
+	}
+
+	
+	
 
 
 }

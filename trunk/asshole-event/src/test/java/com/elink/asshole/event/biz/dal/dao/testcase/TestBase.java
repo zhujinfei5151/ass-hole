0ab31195f7e15daf82f8bc200bcbc8c0357a141e
robert.freeman.mm@gmail.com <robert.freeman.mm@gmail.com@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-04 13:56:21 +0000

    配置文件

---
+++ trunk/asshole-event/src/test/java/com/elink/asshole/event/biz/dal/dao/testcase/TestBase.java
@@ -0,0 +1,42 @@
+package com.elink.asshole.event.biz.dal.dao.testcase;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.springframework.context.ApplicationContext;
+import org.unitils.UnitilsJUnit4;
+import org.unitils.spring.annotation.SpringApplicationContext;
+
+import com.taobao.hsf.hsfunit.HSFEasyStarter;
+
+public abstract class TestBase extends UnitilsJUnit4 {
+
+	protected final Log log = LogFactory.getLog(getClass());
+
+    static {
+        HSFEasyStarter.startFromPath("D:\\hsf\\hsfunit\\1.4.8\\taobao-hsf.sar");
+    }
+
+	@Before
+	public void before() {
+		log.debug("----" + getClass().getSimpleName() + "---");
+	}
+
+	// 
+	@SpringApplicationContext({ "classpath:springbeans-resource.xml", "classpath:dal-dao.xml" })
+	protected static ApplicationContext ctx;
+
+	protected String join(List<?> list) {
+		StringBuilder sb = new StringBuilder();
+		for (Object tmp : list) {
+			sb.append(tmp).append(",");
+		}
+		if (sb.length() > 0) {
+			sb.deleteCharAt(sb.length() - 1);
+		}
+		return sb.toString();
+	}
+
+}

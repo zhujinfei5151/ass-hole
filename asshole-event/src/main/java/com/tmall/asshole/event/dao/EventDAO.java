61aca2b2e8f5b2245590595572944da05cf90ab2
robert.freeman.mm <robert.freeman.mm@gmail.com>
2012-06-04 13:41:55 +0000

    重构

---
+++ asshole-event/src/main/java/com/tmall/asshole/event/dao/EventDAO.java
@@ -0,0 +1,48 @@
+package com.tmall.asshole.event.dao;
+
+import java.util.List;
+
+import com.taobao.common.dao.persistence.exception.DAOException;
+import com.tmall.asshole.event.common.Event;
+
+public interface EventDAO {
+
+	/**
+	 * 插入数据
+	 * @return 插入数据的主锄1�7
+	 * @throws DAOException 
+	 */
+	public Long insertEventDO(Event eventDo) throws DAOException;
+
+	/**
+	 * 更新记录
+	 * @return 受影响的行数
+	 */
+	public Integer updateEvent(Event eventDo)throws DAOException;
+
+	/**
+	 * 查询
+	 * 
+	 * @return
+	 */
+	public List<Event> queryEvent(int start, int end, int count, String env)throws DAOException;
+
+	/**
+	 * 批量修改状�1�7�1�7
+	 * @return
+	 */
+	public Integer batchChangeEventStatus(int oldSatus, int newStatus)throws DAOException;
+
+	/**
+	 * 批量修改
+	 * @return
+	 */
+	public Integer batchChangeEventStatusBytime(int from, int to,int minute)throws DAOException;
+
+}
+
+
+
+
+
+

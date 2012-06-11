8edcc67700125d39ba110261fe41a4bd01529d20
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-11 12:54:56 +0000

    代码重构ing

--- trunk/asshole-engine/src/test/java/com/tmall/asshole/schedule/process/EventSchedulerProcessorTest.java
+++ trunk/asshole-engine/src/test/java/com/tmall/asshole/schedule/process/EventSchedulerProcessorTest.java
@@ -1,21 +1,35 @@
 package com.tmall.asshole.schedule.process;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.tmall.asshole.engine.process.EventSchedulerProcessor;
+import com.tmall.asshole.ep.TestEvent1;
 
 /***
  * 
  * @author tangjinou
- *
+ * 
  */
 public class EventSchedulerProcessorTest {
-	
+	/***
+	 * 测试需要启动Spring容器
+	 */
 	@Test
-	public void testEventSchedulerProcessor(){
-		
-		EventSchedulerProcessor processor = new EventSchedulerProcessor();
-		
+	public void testEventSchedulerProcessor() {
+			ApplicationContext context = new ClassPathXmlApplicationContext(
+					"config.xml");
+			EventSchedulerProcessor processor = (EventSchedulerProcessor) context
+					.getBean("eventSchedulerProcessor");
+	   try {
+			processor.process(new TestEvent1());
+		} catch (Exception e) {
+			Assert.fail();
+		}
+
 	}
 
 }

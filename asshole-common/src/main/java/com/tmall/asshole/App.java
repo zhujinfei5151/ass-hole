d78998a99647660ce05e516713347a42788539af
tangjinou <tangjinou@gmail.com>
2012-06-11 02:56:43 +0000

    增加新工程

---
+++ asshole-common/src/main/java/com/tmall/asshole/App.java
@@ -0,0 +1,13 @@
+package com.tmall.asshole;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

a3cb7c65c06d781f177c9c3e48d8522c11b7a77c
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-12 01:38:21 +0000

    代码重构ing

---
+++ asshole-common/src/main/java/com/tmall/asshole/common/ScheduleType.java
@@ -0,0 +1,46 @@
+package com.tmall.asshole.common;
+
+/****
+ * @author tangjinou
+ */
+public enum ScheduleType {
+     //正常调度器
+	BASIC_SCHEDULE(0, "正常调度器l"), 
+	
+	DELAY_SCHEDULE(1, "延期调度器");
+
+	private int code;
+	private String name;
+
+	private ScheduleType(int code, String name) {
+		this.code = code;
+		this.name = name;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static final ScheduleType getEventEnvByCode(Integer code) {
+		ScheduleType t = null;
+		for (ScheduleType e : ScheduleType.values()) {
+			if (code.intValue() == e.getCode()) {
+				t = e;
+				break;
+			}
+		}
+		return t;
+	}
+}

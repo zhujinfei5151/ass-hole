1258b28a1b4d3f13b5fae9cf4adb16438f71d442
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-12 01:10:43 +0000

    代码重构ing

---
+++ trunk/asshole-common/src/main/java/com/tmall/asshole/common/EventEnv.java
@@ -0,0 +1,48 @@
+package com.tmall.asshole.common;
+
+/****
+ * @author tangjinou
+ */
+public enum EventEnv {
+
+	DEV(0, "dev"), 
+	DAILY(1, "daily"), 
+	PRE(2, "pre"), 
+	ONLINE(3, "online"), 
+	LOCAL(5, "local");
+
+	private int code;
+	private String name;
+
+	private EventEnv(int code, String name) {
+		this.code = code;
+		this.name = name;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static final EventEnv getEventEnvByCode(Integer code) {
+		EventEnv t = null;
+		for (EventEnv e : EventEnv.values()) {
+			if (code.intValue() == e.getCode()) {
+				t = e;
+				break;
+			}
+		}
+		return t;
+	}
+}

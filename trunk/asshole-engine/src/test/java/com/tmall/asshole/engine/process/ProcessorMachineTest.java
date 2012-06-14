06be57b331f2efc76f66e79c2047064b0f05a1fb
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-14 06:05:25 +0000

    代码重构ing

--- trunk/asshole-engine/src/test/java/com/tmall/asshole/engine/process/ProcessorMachineTest.java
+++ trunk/asshole-engine/src/test/java/com/tmall/asshole/engine/process/ProcessorMachineTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class ProcessorProxyTest {
+public class ProcessorMachineTest {
 	/***
 	 * 测试需要启动Spring容器
 	 */

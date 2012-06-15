3106487d8a29aa16737c80d01f53c2996cb09263
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-15 07:13:53 +0000

    代码重构ing

--- asshole-engine/src/test/java/com/tmall/asshole/mock/dao/MockEventDAO.java
+++ asshole-engine/src/test/java/com/tmall/asshole/mock/dao/MockEventDAO.java
@@ -11,40 +11,36 @@ public class MockEventDAO implements IEventDAO{
 	
 	Stack<Event> s =new Stack<Event>();
 	
-	@Override
 	public Integer updateServiceEventDO(Event dao) {
 		return 1;
 	}
 	
-	@Override
 	public Event queryEventByPrimaryKey(Long id, Integer hashNum) {
 		// 暂时不需要
 		return null;
 	}
-	
-	@Override
 	public Integer insertServiceEventDO(Event dao) {
 	    boolean result = s.add(dao);
 	    return result==true?1:0;
 	}
 	
-	@Override
 	public Integer batchChangeEventStatusBytime(int from, int to, int minute) {
 		// 暂时不需要
 		return null;
 	}
 	
-	@Override
 	public Integer batchChangeEventStatus(int from, int to) {
 		// 暂时不需要
 		return null;
 	}
 
-	@Override
+
 	public List<Event> queryEvent(int start, int end, int count,
 			int env, int scheduleType) {
 		List<Event> l= new ArrayList<Event>();
-		l.add(s.pop());
+		if(s.size()>0){
+		   l.add(s.pop());
+		}
 		return l;
 	}
 }
--- asshole-engine/src/test/java/com/tmall/asshole/schedule/ScheduleTest.java
+++ asshole-engine/src/test/java/com/tmall/asshole/schedule/ScheduleTest.java
@@ -8,8 +8,11 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.tmall.asshole.common.Event;
+import com.tmall.asshole.common.EventContext;
 import com.tmall.asshole.config.EngineConfig;
 import com.tmall.asshole.engine.process.EventSchedulerProcessor;
+import com.tmall.asshole.mock.dao.MockEventDAO;
 import com.tmall.asshole.schedule.fgetcplolicy.BasicScheduleFgetcPolicy;
 import com.tmall.asshole.zkclient.ZKConfig;
 
@@ -29,13 +32,16 @@ public class ScheduleTest {
 		config.setGroupingName("testGroup");
 		config.setSchedulingPollingTime(600);
 		config.setMaxHashNum(10000);
+		config.setScheduleType("BASIC_SCHEDULE");
 	}
 	
 	
 	@Test
 	public void testSchedule(){
 		try{
-		  Schedule schedule= new Schedule(processor,processor,config);
+		  processor.setScheduleType("BASIC_SCHEDULE");
+		  processor.setEventDAOForTest(new MockEventDAO());
+		  Schedule<Event,EventContext> schedule= new Schedule<Event,EventContext>(processor,processor,processor,config);
 		  BasicScheduleFgetcPolicy scheduleFgetcPolicy = (BasicScheduleFgetcPolicy)schedule.getScheduleFgetcPolicy();
 		  List<String> machines = new ArrayList<String>();
 		  machines.add("192.168.1.23");
@@ -45,7 +51,7 @@ public class ScheduleTest {
 		  scheduleFgetcPolicy.onChange(machines);
 		  schedule.strart();
 		  Assert.assertTrue(scheduleFgetcPolicy.getEndIndex() > 0);
-		  Thread.sleep(300000);
+		  Thread.sleep(3000);
 		}catch (Exception e) {
 			Assert.fail();
 		}

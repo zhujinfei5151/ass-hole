a4b181f694abf6bf07e836637bd09064ccb00df4
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-17 13:44:50 +0000

    代码重构ing

--- trunk/asshole-test/src/main/java/com/tmall/asshole/maintest/Main.java
+++ trunk/asshole-test/src/main/java/com/tmall/asshole/maintest/Main.java
@@ -1,10 +1,10 @@
 package com.tmall.asshole.maintest;
 
-import org.junit.Assert;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.tmall.asshole.engine.process.EventSchedulerProcessor;
+import com.tmall.asshole.engine.process.ProcessorMachine;
+import com.tmall.asshole.mock.ep.TestEvent1;
 
 public class Main {
 	/**
@@ -12,13 +12,21 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		try {
-			ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+			 ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 			
-			 EventSchedulerProcessor processor = (EventSchedulerProcessor) context.getBean("eventSchedulerProcessor");
-			
+			 ProcessorMachine   machine= (ProcessorMachine)context.getBean("processorMachine");
+			 
+			 TestEvent1 testEvent1 = new TestEvent1();
+			 
+			 testEvent1.setTestVar1("testVar1");
+			 
+			 machine.createEventProcess(testEvent1, "order_card");
+			 
+			 
 			 Thread.sleep(1000000);
-		} catch (InterruptedException e) {
-			Assert.fail();
+		} catch (Exception e) {
+			e.printStackTrace();
+			//Assert.fail();
 		}
 
 	}

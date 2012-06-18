0892906bb051897882b73a0c1938c9599d57bbf0
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-18 11:24:59 +0000

    代码重构ing

--- asshole-engine/src/test/java/com/tmall/asshole/mock/dao/MockEventDAO.java
+++ asshole-engine/src/test/java/com/tmall/asshole/mock/dao/MockEventDAO.java
@@ -7,42 +7,44 @@ import java.util.Stack;
 import com.tmall.asshole.common.Event;
 import com.tmall.asshole.common.IEventDAO;
 
-public class MockEventDAO implements IEventDAO{
-	
-	Stack<Event> s =new Stack<Event>();
-	
-	public Integer updateServiceEventDO(Event dao) {
-		return 1;
-	}
-	
+public class MockEventDAO implements IEventDAO {
+
+	Stack<Event> s = new Stack<Event>();
+
 	public Event queryEventByPrimaryKey(Long id, Integer hashNum) {
 		// 暂时不需要
 		return null;
 	}
-	public Integer insertServiceEventDO(Event dao) {
-	    boolean result = s.add(dao);
-	    return result==true?1:0;
-	}
-	
+
 	public Integer batchChangeEventStatusBytime(int from, int to, int minute) {
 		// 暂时不需要
 		return null;
 	}
-	
+
 	public Integer batchChangeEventStatus(int from, int to) {
 		// 暂时不需要
 		return null;
 	}
 
-
-	public List<Event> queryEvent(int start, int end, int count,
-			int env, int scheduleType) {
-		List<Event> l= new ArrayList<Event>();
-		if(s.size()>0){
-			if(s.peek().getSchedule_type()==scheduleType){
+	public List<Event> queryEvent(int start, int end, int count, int env,
+			int scheduleType) {
+		List<Event> l = new ArrayList<Event>();
+		if (s.size() > 0) {
+			if (s.peek().getSchedule_type() == scheduleType) {
 				l.add(s.pop());
 			}
 		}
 		return l;
+	}
+
+	@Override
+	public Integer insertEventDO(Event dao) {
+		boolean result = s.add(dao);
+		return result == true ? 1 : 0;
+	}
+
+	@Override
+	public Integer updateEventDO(Event dao) {
+		return 1;
 	}
 }

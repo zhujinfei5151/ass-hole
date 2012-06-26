cdd062a29e86d53d775e10e644c0480baf3689d0
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-06-26 10:41:17 +0000

    代码重构ing

--- asshole-common/src/main/java/com/tmall/asshole/common/EventStatus.java
+++ asshole-common/src/main/java/com/tmall/asshole/common/EventStatus.java
@@ -2,13 +2,13 @@ package com.tmall.asshole.common;
 
 public enum EventStatus {
 
-	EVENT_STATUS_FAILED(-1, "ִ��ʧ��"),
-	EVENT_STATUS_UNEXECUTED(0, "δִ��"),
-	EVENT_STATUS_SUCCESS(1, "ִ�гɹ�"), 
-	EVENT_STATUS_DELETED(2, "ִ�б�ɾ��"), 
-	EVENT_STATUS_PARAMETER_ERROR(3, "�����쳣"), 
-	EVENT_STATUS_LOADED(9, "ִ����"), 
-	EVENT_STATUS_EXCEPTION(10, "ִ���쳣");
+	EVENT_STATUS_FAILED(-1, "处理失败"),
+	EVENT_STATUS_UNEXECUTED(0, "未执行"),
+	EVENT_STATUS_SUCCESS(1, "处理成功"), 
+	EVENT_STATUS_DELETED(2, "删除"), 
+	EVENT_STATUS_PARAMETER_ERROR(3, "处理中参数异常"), 
+	EVENT_STATUS_LOADED(9, "处理中"), 
+	EVENT_STATUS_EXCEPTION(10, "处理异常");
 
 	private int code;
 

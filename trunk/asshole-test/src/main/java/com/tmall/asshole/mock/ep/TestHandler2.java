3d764fad40e46f1c13a225b460dad84d947e8412
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-07-13 02:03:39 +0000

    代码重构ing

--- trunk/asshole-test/src/main/java/com/tmall/asshole/mock/ep/TestHandler2.java
+++ trunk/asshole-test/src/main/java/com/tmall/asshole/mock/ep/TestHandler2.java
@@ -1,13 +1,15 @@
 package com.tmall.asshole.mock.ep;
 
-import com.tmall.asshole.common.Event;
 import com.tmall.asshole.common.EventContext;
 import com.tmall.asshole.engine.AbstractHandler;
 
-public class TestHandler2 extends AbstractHandler {
+public class TestHandler2 extends AbstractHandler<TestEvent2,EventContext> {
 
-	public boolean handle(Event event, EventContext context) throws Exception {
-		return true;
+	public boolean handle(TestEvent2 event, EventContext context)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return false;
 	}
+
 
 }

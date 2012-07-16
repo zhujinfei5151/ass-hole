b7a0a4e1ddcf96e679846b26bb7ea1e7543a16d2
tangjinou <tangjinou@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-07-16 23:52:23 +0000

    代码重构ing

--- asshole-test/src/main/java/com/tmall/asshole/mock/ep/TestHandler2.java
+++ asshole-test/src/main/java/com/tmall/asshole/mock/ep/TestHandler2.java
@@ -1,5 +1,10 @@
 package com.tmall.asshole.mock.ep;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.tmall.asshole.common.EventContext;
 import com.tmall.asshole.engine.AbstractHandler;
 
@@ -7,8 +12,25 @@ public class TestHandler2 extends AbstractHandler<TestEvent2,EventContext> {
 
 	public boolean handle(TestEvent2 event, EventContext context)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		
+		 List<Map<String,Object>> dataList = new ArrayList<Map<String,Object>>();
+		
+		 Map<String,Object> map1=new HashMap<String,Object>();
+		 
+		 map1.put("testVar3", "jiuxian.tjo 1");
+		 
+		 dataList.add(map1);
+		 
+		 Map<String,Object> map2=new HashMap<String,Object>();
+		 
+         map1.put("testVar3", "jiuxian.tjo 2");
+		 
+		 dataList.add(map2);
+		
+		context.putDataList(dataList);
+	
+		
+		return true;
 	}
 
 

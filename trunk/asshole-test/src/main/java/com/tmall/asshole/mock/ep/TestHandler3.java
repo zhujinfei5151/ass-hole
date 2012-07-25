c00a87cd0258200ec6a5c4abed55490b7f1f40d8
tangjinou@gmail.com <tangjinou@gmail.com@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-07-25 01:06:29 +0000

    去除事件的标示

--- trunk/asshole-test/src/main/java/com/tmall/asshole/mock/ep/TestHandler3.java
+++ trunk/asshole-test/src/main/java/com/tmall/asshole/mock/ep/TestHandler3.java
@@ -1,8 +1,11 @@
 package com.tmall.asshole.mock.ep;
 
+import org.springframework.stereotype.Component;
+
 import com.tmall.asshole.common.EventContext;
 import com.tmall.asshole.engine.AbstractHandler;
 
+@Component
 public class TestHandler3  extends AbstractHandler<TestEvent3,EventContext>{
 
 	public boolean handle(TestEvent3 event, EventContext context)

567f1491fef893c27c48afdc03de6b0257762802
tangjinou@gmail.com <tangjinou@gmail.com@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-08-06 06:04:49 +0000

    增加同步处理的方式

---
+++ trunk/asshole-engine/src/main/java/com/tmall/asshole/engine/http/JettyServer.java
@@ -0,0 +1,46 @@
+package com.tmall.asshole.engine.http;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import com.tmall.asshole.engine.http.screen.RunTimeServlet;
+import com.tmall.asshole.engine.process.ProcessorMachine;
+
+public class JettyServer{
+	
+	private static transient Log logger = LogFactory.getLog(JettyServer.class);
+	
+	private ProcessorMachine processorMachine;
+	
+	public JettyServer(ProcessorMachine processorMachine){
+		this.processorMachine = processorMachine;
+	}
+	
+	public void start() throws Exception{
+		Server server = new Server(9999);  
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);  
+        context.setContextPath("/asshole");  
+        server.setHandler(context);
+        
+        RunTimeServlet runTimeServlet = new RunTimeServlet();
+        processorMachine.getScheduleMonitor().addWatcher(runTimeServlet);
+        
+        context.addServlet(new ServletHolder(runTimeServlet), "/runtime");  
+        logger.info("start the jetty server,port= 9999");
+		server.start();
+		server.join();  
+	}
+	
+//	public static void main(String[] args) {
+//		try {
+//			new JettyServer().start();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+}

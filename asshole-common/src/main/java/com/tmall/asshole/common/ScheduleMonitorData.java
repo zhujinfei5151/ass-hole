2aaaf5114b1b31b0cfa40836eca2c55c47bf1431
tangjinou <tangjinou@gmail.com>
2012-08-06 06:03:48 +0000

    增加同步处理的方式

--- asshole-common/src/main/java/com/tmall/asshole/common/ScheduleMonitorData.java
+++ asshole-common/src/main/java/com/tmall/asshole/common/ScheduleMonitorData.java
@@ -1,5 +1,10 @@
 package com.tmall.asshole.common;
+
+import java.util.List;
+
 /**
+ * 
+ * 监控的数据结构
  * 
  * @author tangjinou (jiuxian.tjo)
  *
@@ -7,6 +12,11 @@ package com.tmall.asshole.common;
 public class ScheduleMonitorData {
 
 	private long countOfUnExecuteEvent;
+	
+	private String env;
+	
+	private List<String> machines;
+	
 
 	public long getCountOfUnExecuteEvent() {
 		return countOfUnExecuteEvent;
@@ -19,10 +29,27 @@ public class ScheduleMonitorData {
 	public ScheduleMonitorData(long countOfUnExecuteEvent) {
 		this.countOfUnExecuteEvent = countOfUnExecuteEvent;
 	}
-	
-	public ScheduleMonitorData(){
-		
+
+	public String getEnv() {
+		return env;
 	}
+
+	public void setEnv(String env) {
+		this.env = env;
+	}
+
+	public List<String> getMachines() {
+		return machines;
+	}
+
+	public void setMachines(List<String> machines) {
+		this.machines = machines;
+	}
+	
+	
+   
+	
+   
 	
 	
 }

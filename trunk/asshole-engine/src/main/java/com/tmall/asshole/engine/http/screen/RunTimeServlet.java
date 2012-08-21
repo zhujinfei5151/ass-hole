4e07fe20293a4e65331d587397f3c0a746b05129
tangjinou@gmail.com <tangjinou@gmail.com@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-08-21 06:29:08 +0000

    代码重构 升级版本1.0.3

--- trunk/asshole-engine/src/main/java/com/tmall/asshole/engine/http/screen/RunTimeServlet.java
+++ trunk/asshole-engine/src/main/java/com/tmall/asshole/engine/http/screen/RunTimeServlet.java
@@ -1,63 +1,47 @@
 package com.tmall.asshole.engine.http.screen;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jetty.continuation.Continuation;
-import org.eclipse.jetty.continuation.ContinuationSupport;
+import org.apache.commons.logging.Log;
 
-import com.tmall.asshole.common.IScheduleMonitorWatcher;
-import com.tmall.asshole.common.ScheduleMonitorData;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.tmall.asshole.common.LoggerInitUtil;
+import com.tmall.asshole.engine.process.ProcessorMachine;
 
 
 
-public class RunTimeServlet extends HttpServlet implements IScheduleMonitorWatcher{
-	
+public class RunTimeServlet extends HttpServlet{
+	    
+	    private final static Log logger = LoggerInitUtil.LOGGER;
 	
 	    private static final long serialVersionUID = 1L;
-		private ScheduleMonitorData data;  
-	  
-	  
-	    protected void doGet(HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {  
-	    	final Continuation continuation = ContinuationSupport.getContinuation(request);
-	    	response.setContentType("text/html;charset=UTF-8");
-	    	response.setBufferSize(0);
-	    	
-	    	  new Thread(new Runnable() {
-	              public void run() {
-	                  System.out.println("begin");
-	                  try {
-	                	  OutputStream out = response.getOutputStream();
-	                      for (int i = 0; i < 1000; ++i) {
-	                          System.out.println("~~~~~~~~~~ok");
-	                          out.write(("data=" + data.getCountOfUnExecuteEvent()+",").getBytes());
-	                          out.flush();
-	                          Thread.sleep(1000);
-	                      }
 
-	                  } catch (Exception e1) {
-	                      e1.printStackTrace();
-	                  }
-	                  continuation.resume();
-	                  System.out.println("end");
-	              }
-	          }).start();
+		private ProcessorMachine processorMachine;
+	    
+	  
+	    public RunTimeServlet(ProcessorMachine processorMachine) {
+			this.processorMachine=processorMachine;
+		}
 
-	          continuation.setTimeout(100000000);
-	          continuation.suspend();
+
+
+		protected void doGet(HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {  
+	  	      
+			  SerializeConfig config=new SerializeConfig();
+			  config.setAsmEnable(false);
+			  
+	    	  response.getWriter().println(JSON.toJSONString(processorMachine.getScheduleMonitor().getScheduleMonitorData(),config,SerializerFeature.WriteClassName));
 	        
 	        
 	    }
 
-	    @Override
-		public void onChange(ScheduleMonitorData data) {
-			this.data= data;
-		}  
-
+	    
 	    
 }

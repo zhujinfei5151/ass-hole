ab409b34f7ec0a81050e2c2ff6f33ffd23e38b9a
robert.freeman.mm@gmail.com <robert.freeman.mm@gmail.com@c80d1f51-ea52-3774-b90d-e8da89561337>
2012-09-03 07:36:26 +0000

    添加 返回当前节点的上一个节点

--- trunk/asshole-common/src/main/java/com/tmall/asshole/common/IEventDAO.java
+++ trunk/asshole-common/src/main/java/com/tmall/asshole/common/IEventDAO.java
@@ -35,8 +35,11 @@ public interface IEventDAO {
     public List<Event> queryEventList(int processInstanceId);
     
     /**
-     * 根据当前节点 返回流程中上一个节点
+     * 根据当前节点 返回流程中上一个节点 
+     * <br>如果上一个节点存在，则正常返回 ；</br>
+     * <br>如果当前节点已经是第一个节点，返回本身；</br>
+     * <br>如果当前节点不存在，返回null</br>
      */
-   // public Event queryLastNodeEvent(int processInstanceId,String currentNode);
+    public Event queryLastNodeEvent(int processInstanceId,String currentNode);
 
 }

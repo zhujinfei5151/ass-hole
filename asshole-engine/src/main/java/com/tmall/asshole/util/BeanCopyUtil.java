4771b4696c3b5eced09e9fc4dc7dbb4e512560f4
tangjinou <tangjinou@gmail.com>
2012-09-12 09:12:37 +0000

    代码重构ing

--- asshole-engine/src/main/java/com/tmall/asshole/util/BeanCopyUtil.java
+++ asshole-engine/src/main/java/com/tmall/asshole/util/BeanCopyUtil.java
@@ -16,6 +16,19 @@ public class BeanCopyUtil {
 				try{
 					// 现在只拷贝一级
 					Field field = o.getClass().getDeclaredField(entry.getKey());
+					
+					Class<?> parent =  o.getClass();
+					while(field ==null && parent.getSuperclass()!=null){
+						if(isClassContainsFiled(entry.getKey(),parent.getSuperclass().getDeclaredFields())){
+							   field = parent.getSuperclass().getDeclaredField(entry.getKey());
+						}
+						parent = parent.getSuperclass();
+					}
+					
+					if(field==null){
+						continue;
+					}
+					
 					Object value = entry.getValue();
 					//只有名字和类型一直时候才拷贝
 					if(field.getType() ==value.getClass())
@@ -28,7 +41,15 @@ public class BeanCopyUtil {
 				}
 		}
 		
-		
+	}
+	
+	public static boolean isClassContainsFiled(String name,Field[] declaredFields){
+		for (Field f : declaredFields) {
+			if(f.toString().trim().contains(name)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
